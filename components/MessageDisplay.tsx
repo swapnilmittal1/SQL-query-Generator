d41bb0077fd7928583f833d85406611346fbe1a0
Swapnil Mittal <72509297+swapnilmittal1@users.noreply.github.com>
2023-09-11 18:54:52 -0400

    Update MessageDisplay.tsx

--- components/MessageDisplay.tsx
+++ components/MessageDisplay.tsx
@@ -1,9 +1,20 @@
-const MessageDisplay = () => {
-    return (
-        <div className="message-display">
+import React from 'react';
 
-        </div>
-    );
+interface MessageDisplayProps {
+  messages: string[];
 }
+
+const MessageDisplay: React.FC<MessageDisplayProps> = ({ messages }) => {
+  return (
+    <div className="message-display">
+      <h2>Messages:</h2>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default MessageDisplay;
