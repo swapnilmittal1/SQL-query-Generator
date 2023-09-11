5143374165fef8314e2cba7a49533557e1cc4a8a
Swapnil Mittal <72509297+swapnilmittal1@users.noreply.github.com>
2023-09-11 18:53:08 -0400

    CodeDisplay.tsx

--- components/CodeDisplay.tsx
+++ components/CodeDisplay.tsx
@@ -1,7 +1,13 @@
-const CodeDisplay = () => {
+import React from 'react';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+const CodeDisplay = ({ codeString }) => {
     return (
         <div className="code-display">
-
+            <SyntaxHighlighter language="javascript" style={docco}>
+                {codeString}
+            </SyntaxHighlighter>
         </div>
     );
 }
